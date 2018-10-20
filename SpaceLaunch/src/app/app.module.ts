b7350c36e0a9dc857582d997b5fc4c34ff31beda
Melnikov Maxim <maxmelnikov37@gmail.com>
2018-10-20 16:09:29 +0300

    Add component

--- SpaceLaunch/src/app/app.module.ts
+++ SpaceLaunch/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListOfLaunchesComponent } from './list-of-launches/list-of-launches.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ListOfLaunchesComponent
+   ],
+   imports: [
+      BrowserModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ SpaceLaunch/src/app/list-of-launches/list-of-launches.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-of-launches',
+  templateUrl: './list-of-launches.component.html',
+  styleUrls: ['./list-of-launches.component.css']
+})
+export class ListOfLaunchesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
