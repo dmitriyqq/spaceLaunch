c06e852c194e9c5a340a2357193068cd05d9f081
maksim.melnikov <maksim.melnikov@akvelon.com>
2018-10-21 00:32:41 +0300

    created launch-detail component

--- SpaceLaunch/src/app/app.module.ts
+++ SpaceLaunch/src/app/app.module.ts
@@ -3,18 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { YearsComponentComponent } from './years-component/years-component.component';
+import { LaunchDetailsComponent } from './launch-details/launch-details.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        YearsComponentComponent
-    ],
-    imports: [
-        BrowserModule
-    ],
-    providers: [],
-    bootstrap: [
-        AppComponent
-    ]
+   declarations: [
+      AppComponent,
+      YearsComponentComponent,
+      LaunchDetailsComponent
+   ],
+   imports: [
+      BrowserModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
